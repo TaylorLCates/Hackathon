8353fbf4903750125c4aa4694d7c47fd22784b4f
Ethan Miller <Ethan.Miller@improving.com>
2019-10-18 09:42:03 -0500

    remove workspace and add WorldFactory

---
+++ src/main/java/org/improving/WorldFactory.java
@@ -0,0 +1,14 @@
+package org.improving;
+
+public class WorldFactory {
+    public Location buildWorld() {
+
+        cc = new Location("Comic Con", gs);
+        gs = new Location("Game Shop", mb);
+        mb = new Location("Mom's Basement", mf);
+        mf = new Location("To-Scale Millenium Falcon Replica", cp);
+        cp = new Location("Generic City Park", fd);
+        fd = new Location("Final Destination", );
+
+    }
+}
