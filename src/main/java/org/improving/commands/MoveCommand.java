151595735dc7d6afae63a97c0c533f445c254d24
Ethan Miller <Ethan.Miller@improving.com>
2019-10-18 11:52:15 -0500

    simplify move command

--- src/main/java/org/improving/commands/MoveCommand.java
+++ src/main/java/org/improving/commands/MoveCommand.java
@@ -4,26 +4,14 @@ import org.improving.GameContext;
 import org.improving.InputOutput;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-
 @Component
 public class MoveCommand extends BaseAliasCommand {
     public MoveCommand(InputOutput io) {
-        super(io, "move", "m");
-    }
-
-    @Override
-    public boolean isValid(String input, GameContext gameContext) {
-        if (input.split(" ").length < 2) return false;
-        return input.split(" ")[0].equalsIgnoreCase("move");
+        super(io, "move", "m", "mo", "mov", "teleport");
     }
 
     @Override
     public void execute(String input, GameContext gameContext) {
-        // TODO: remove precious block of code beneath that will only be used if we give Location a list of exits
-        var parts = input.split(" ");
-        var destArr = Arrays.copyOfRange(parts, 1, parts.length);
-        var dest = String.join(" ", destArr);
 
         var location = gameContext.getPlayer().getLocation();
         if (location.getAdversary() != null) {
