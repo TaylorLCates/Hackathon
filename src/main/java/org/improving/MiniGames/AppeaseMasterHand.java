222be236372c455cd62d8cf93c62f6234a848bfd
Rachel Sullivan <Rachel.Sullivan@improving.com>
2019-10-18 12:34:59 -0500

    Added Guess Cheat Code Mini Game stuff

--- src/main/java/org/improving/MiniGames/AppeaseMasterHand.java
+++ src/main/java/org/improving/MiniGames/AppeaseMasterHand.java
@@ -1,9 +1,34 @@
 package org.improving.MiniGames;
 
+import org.improving.domain.Item;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Component
 public class AppeaseMasterHand {
 
+//    Item item ;
+//    Random rand = new Random();
+//    ArrayList<String> itemOrder = new ArrayList<>();
+//
+//    itemOrder.add
+//
+//    items
+//
+//    itemOrder
+//
+//
+
+
+
     public void run(){}
+
+
+
+
+
+
 }
